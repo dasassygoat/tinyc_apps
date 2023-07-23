4aa727c2ffc818bab52eb8f95d5df110f9ada69c
journeyman32 <bryan@coderbox.co>
2023-07-23 03:08:09 -0500

    update gitignore

---
+++ cmake-build-debug/CMakeFiles/tinyc_apps.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tinyc_apps.dir/nato02.c.o"
+  "CMakeFiles/tinyc_apps.dir/nato02.c.o.d"
+  "tinyc_apps"
+  "tinyc_apps.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tinyc_apps.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
