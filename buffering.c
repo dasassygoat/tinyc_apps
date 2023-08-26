a61975cddf96891f06c41de5f9153032394a5adf
journeyman32 <bryan@coderbox.co>
2023-08-25 21:05:03 -0500

    buffering

---
+++ buffering.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    char buffer[BUFSIZ];
+    int a,b;
+
+    setbuf(stdout,buffer);
+
+    fflush(stdout);
+
+    printf("Type a letter:");
+    a = getchar();
+    printf("Type a letter:");
+    b = getchar();
+
+    printf("a='%c', b='%c'\n",a,b);
+
+    return(0);
+}
