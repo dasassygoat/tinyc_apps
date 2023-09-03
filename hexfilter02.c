faaf28f30b077c0294e018b61d3531fcaf05a1bd
journeyman32 <bryan@coderbox.co>
2023-09-03 00:39:19 -0500

    exercise 4.6
    Signed-off-by: journeyman32 <bryan@coderbox.co>

--- hexfilter02.c
+++ hexfilter02.c
@@ -3,13 +3,25 @@
 int main(){
     int ch,count=0;
     int screenlength = 80;
-
+    for(int i=0; i<80;i++){
+        printf("-");
+    }
+ printf("\n");
     while ((ch=getchar()) != EOF){
         if(ch == '\n'){printf("\n"); count = 0;}
         count++;
-        printf("%02x ", ch);
+        if(count <= 26){
+            printf("%02x ", ch);
+        }
+        else if (count == 27){
+            printf("%02x", ch);
+        }
+        else if (count > 27){
+            printf("\n%02x ", ch);
+            count = 1;
+        }
+        
         
     }
-    printf("%i", count);
     return 0;
 }
