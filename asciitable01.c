8ef5b77daccbc43fb91473338309d7ebb8d66ada
journeyman32 <bryan@coderbox.co>
2023-09-10 01:04:45 -0500

    display dash for dec less 33
    Signed-off-by: journeyman32 <bryan@coderbox.co>

--- asciitable01.c
+++ asciitable01.c
@@ -3,7 +3,13 @@
 int main(){
     printf("Dec\tOct\tHex\tC\t\n");
     for(int x = 0; x <= 127; x++){
-        printf("%d\t%o\t%x\t%c\t\n", x, x, x, x);
+        if (x<33){
+            printf("%d\t%o\t%x\t-\t\n", x, x, x);
+        }
+        else{
+            printf("%d\t%o\t%x\t%c\t\n", x, x, x, x);
+        }
+        
     }
     return 0;
 }
