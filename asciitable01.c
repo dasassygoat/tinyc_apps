8142756dd5be85536a194d5838c435d41956cc64
journeyman32 <bryan@coderbox.co>
2023-09-10 01:24:13 -0500

    correct format
    Signed-off-by: journeyman32 <bryan@coderbox.co>

--- asciitable01.c
+++ asciitable01.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    printf("Dec\tOct\tHex\tC\t\n");
-    for(int x = 0; x <= 127; x++){
-        if (x<33){
-            printf("%d\t%o\t%x\t-\t\n", x, x, x);
-        }
-        else{
-            printf("%d\t%o\t%x\t%c\t\n", x, x, x, x);
-        }
-        
+    printf("Dec\tOct\tHex\tC\t\tDec\tOct\tHex\tC\t\tDec\tOct\tHex\tC\tDec\t\tOct\tHex\tC\t\n");
+    for(int x = 0; x <= 31; x++){
+            printf("%d\t%o\t%x\t%c\t|\t%d\t%o\t%x\t%c\t|\t%d\t%o\t%x\t%c\t|\t%d\t%o\t%x\t%c\t\n", x, x, x,x, x+32,x+32,x+32,x+32,x+64,x+64,x+64,x+64,x+96,x+96,x+96,x+96);
+       
     }
     return 0;
 }
