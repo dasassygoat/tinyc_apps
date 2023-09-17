a0878792cffb1fb3b9cbb15b7699597e05810285
journeyman32 <bryan@coderbox.co>
2023-09-16 21:24:52 -0500

    asciitable
    Signed-off-by: journeyman32 <bryan@coderbox.co>

--- asciitable01.c
+++ asciitable01.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 int main(){
-    printf("Dec\tOct\tHex\tC\t\tDec\tOct\tHex\tC\t\tDec\tOct\tHex\tC\tDec\t\tOct\tHex\tC\t\n");
+    printf("Dec\tOct\tHex\tC\t Dec\tOct\tHex\tC\t Dec\tOct\tHex\tC\t Dec\tOct\tHex\tC\n");
     for(int x = 0; x <= 31; x++){
-            printf("%d\t%o\t%x\t%c\t|\t%d\t%o\t%x\t%c\t|\t%d\t%o\t%x\t%c\t|\t%d\t%o\t%x\t%c\t\n", x, x, x,x, x+32,x+32,x+32,x+32,x+64,x+64,x+64,x+64,x+96,x+96,x+96,x+96);
+            printf("%d %o %x %c|%d %o %x %c|%d %o %x %c|%d %o %x %c\n", x, x, x,x, x+32,x+32,x+32,x+32,x+64,x+64,x+64,x+64,x+96,x+96,x+96,x+96);
+            char next;
+            scanf("%c", &next);
        
     }
     return 0;
